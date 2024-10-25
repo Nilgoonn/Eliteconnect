29d8e31fa04f207113de6255255e4170def2e354
Nilgoon <Nilgoonnilforoosh@gmail.com>
2024-10-25 13:46:38 +0330

    Initial commit

---
+++ src/main/java/com/paisly/elite_connect/EliteConnectApplication.java
@@ -0,0 +1,13 @@
+package com.paisly.elite_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliteConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliteConnectApplication.class, args);
+	}
+
+}
