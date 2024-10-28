ec023f7eb76ffb7c4ba4571860fecc4dfd30ec10
arvin <arvin.mola21@gmail.com>
2024-10-28 13:54:21 +0330

    Models created (arvin)

--- src/main/java/com/paisly/elite_connect/EliteConnectApplication.java
+++ src/main/java/com/paisly/elite_connect/EliteConnectApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EliteConnectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EliteConnectApplication.class, args);
+
 	}
 
 }
