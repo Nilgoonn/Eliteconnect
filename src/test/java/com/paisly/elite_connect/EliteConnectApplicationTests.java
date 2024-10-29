6e1998531eec040d98bc3556dfdfd9cf270bd7df
Nilgoon <Nilgoonnilforoosh@gmail.com>
2024-10-29 14:22:14 +0330

    Initial commit

---
+++ src/test/java/com/paisly/elite_connect/EliteConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paisly.elite_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliteConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
