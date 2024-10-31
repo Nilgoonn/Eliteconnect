9c77103b1773a6b0a55feee8fe25e522dc22efb7
arvin <arvin.mola21@gmail.com>
2024-10-31 11:34:13 +0330

    models/Controller updated

--- src/main/java/com/paisly/elite_connect/controller/CustomerController.java
+++ src/main/java/com/paisly/elite_connect/controller/CustomerController.java
@@ -1,9 +1,28 @@
 package com.paisly.elite_connect.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.paisly.elite_connect.model.CustomerTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("customer")
+    public String saveCustomer (@RequestBody CustomerTest customer) {
+        customerService.saveCustomer(customer);
+        return "customer saved";
+    }
+
+    @GetMapping("customer/{customerId}")
+    public CustomerTest getCustomerById(@PathVariable int customerId) {
+        customerService.getById(customerId);
+    }
+
+
+
 
 
 
--- src/main/java/com/paisly/elite_connect/model/SimCard.java
+++ src/main/java/com/paisly/elite_connect/model/SimCard.java
@@ -1,25 +1,25 @@
-package com.paisly.elite_connect.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-@Entity
-@Table(name = "simCard")
-@Data
-@RequiredArgsConstructor
-public class SimCard {
-
-    @Id
-    private Long simID;
-    private String simNumber;
-    private String isSold; //previous name : status
-
-    @ManyToOne
-    private SimType typeID;
+//package com.paisly.elite_connect.model;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.ManyToOne;
+//import jakarta.persistence.Table;
+//import lombok.Data;
+//import lombok.RequiredArgsConstructor;
+//
+//@Entity
+//@Table(name = "simCard")
+//@Data
+//@RequiredArgsConstructor
+//public class SimCard {
+//
+//    @Id
+//    private Long simID;
+//    private String simNumber;
+//    private String isSold; //previous name : status
+//
 //    @ManyToOne
-//    private Simprovider providerID;
-}
+//    private SimType typeID;
+////    @ManyToOne
+////    private Simprovider providerID;
+//}
